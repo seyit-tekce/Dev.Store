3f8b1a21d458ccedddb71c7ad3cb577d61589949
Seyit Hamit TEKCE <seyit.tekce@infoline-tr.com>
2023-02-04 15:07:58 +0300

    update

--- src/Dev.Store.Web/abp.resourcemapping.js
+++ src/Dev.Store.Web/abp.resourcemapping.js
@@ -10,5 +10,7 @@ module.exports = {
         "@node_modules/jquery-slugify/dist/**/*": "@libs/jquery-slugify",
         "@node_modules/speakingurl/speakingurl.min.js": "@libs/speakingurl",
         "@node_modules/speakingurl/speakingurl.min.js": "@libs/speakingurl",
+        "@node_modules/@uppy/": "@libs/uppy",
     }
 };
+    
---
+++ src/Dev.Store.Web/wwwroot/custom/js/uppy.custom.js
@@ -0,0 +1,21 @@
+﻿import Uppy from '/libs/uppy/core';
+import XHRUpload from '/libs/uppy/xhr-upload'
+import Dashboard from '/libs/uppy/dashboard'
+
+
+import '/libs/uppy/core/dist/style.css'
+import '/libs/suppy/dashboard/dist/style.css'
+
+
+
+
+
+const uppy = new Uppy()
+    .use(Dashboard, {
+        trigger: '#select-files',
+    })
+    .use(XHRUpload, { endpoint: 'https://api2.transloadit.com' })
+
+uppy.on('complete', (result) => {
+    console.log('Upload complete! We’ve uploaded these files:', result.successful)
+})
