67cb95c7ecfd1d32178525b4e3c5bc307b03b467
Seyit Hamit TEKCE <89526953+seyittekce-infoline@users.noreply.github.com>
2023-02-21 15:11:50 +0300

    tt

--- src/Dev.Store.Web/Pages/Categories/CreateModal.js
+++ src/Dev.Store.Web/Pages/Categories/CreateModal.js
@@ -3,45 +3,45 @@
     },
     functions: {
         uppy: function () {
-            //var uppy = new Uppy.Uppy({
-            //    debug: true,
-            //    autoProceed: false,
-            //    locale: Uppy.locales.tr_TR,
-            //    allowMultipleUploadBatches: false,
-            //    restrictions: {
-            //        maxNumberOfFiles: 1,
-            //        minNumberOfFiles: 1,
-            //        allowedFileTypes: ["image/*"],
-            //        requiredMetaFields: [],
-            //    }
-            //});
-            //uppy.use(Uppy.Dashboard, {
-            //    inline: true,
-            //    locale: Uppy.locales.tr_TR,
-            //    target: '#drag-drop-area',
-            //    showProgressDetails: true,
-            //    browserBackButtonClose: false,
-            //    hideUploadButton: true
-            //}).use(Uppy.XHRUpload, {
-            //    locale: Uppy.locales.tr_TR,
-            //    endpoint: '/Categories/CreateModal/',
-            //    formData: true,
-            //    fieldName: 'Files',
-            //}).use(Uppy.ImageEditor, { target: Uppy.Dashboard })
-            //    .use(Uppy.DropTarget, { target: document.body })
-            //    .use(Uppy.Compressor)
-            //uppy.use(Uppy.Form, {
-            //    locale: Uppy.locales.tr_TR,
-            //    target: '#categoryForm',
-            //    resultName: 'Files',
-            //    getMetaFromForm: true,
-            //    addResultToForm: true,
-            //    submitOnSuccess: false,
-            //    triggerUploadOnSubmit: true,
-            //});
-            //uppy.use(Uppy.Url, {
-            //    // Options
-            //});
+            var uppy = new Uppy.Uppy({
+                debug: true,
+                autoProceed: false,
+                locale: Uppy.locales.tr_TR,
+                allowMultipleUploadBatches: false,
+                restrictions: {
+                    maxNumberOfFiles: 1,
+                    minNumberOfFiles: 1,
+                    allowedFileTypes: ["image/*"],
+                    requiredMetaFields: [],
+                }
+            });
+            uppy.use(Uppy.Dashboard, {
+                inline: true,
+                locale: Uppy.locales.tr_TR,
+                target: '#drag-drop-area',
+                showProgressDetails: true,
+                browserBackButtonClose: false,
+                hideUploadButton: true
+            }).use(Uppy.XHRUpload, {
+                locale: Uppy.locales.tr_TR,
+                endpoint: '/Categories/CreateModal/',
+                formData: true,
+                fieldName: 'Files',
+            }).use(Uppy.ImageEditor, { target: Uppy.Dashboard })
+                .use(Uppy.DropTarget, { target: document.body })
+                .use(Uppy.Compressor)
+            uppy.use(Uppy.Form, {
+                locale: Uppy.locales.tr_TR,
+                target: '#categoryForm',
+                resultName: 'Files',
+                getMetaFromForm: true,
+                addResultToForm: true,
+                submitOnSuccess: false,
+                triggerUploadOnSubmit: true,
+            });
+            uppy.use(Uppy.Url, {
+                // Options
+            });
 
             window.uppy = uppy;
         }
