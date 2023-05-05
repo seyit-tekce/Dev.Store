93fe63fa7a123a722e5ca2ea2adcb835718085ae
Seyit Hamit TEKCE <seyit.tekce@infoline-tr.com>
2023-05-05 10:22:12 +0300

    ss

---
+++ src/Dev.Store.Domain/CartProducts/CartProduct.cs
@@ -0,0 +1,18 @@
+﻿using Dev.Store.Products;
+using System;
+using System.Drawing;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace Dev.Store.CartProducts
+{
+    public class CartProduct : FullAuditedEntity<Guid>
+    {
+        public virtual Guid ProductId { get; set; }
+        public virtual Guid SizeId { get; set; }
+        public virtual double Price { get; set; }
+        public virtual double Amount { get; set; }
+
+        public Product Product { get; set; }
+        public Size Size { get; set; }
+    }
+}
